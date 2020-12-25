02c428cd4a3dce1285a19b4c1d9767aad1bb4801
“e <18790334713@163.com>
2020-12-25 15:03:14 +0800

    宝塔安装

---
+++ 宝塔/install.sh
@@ -0,0 +1,10 @@
+# CentOS 7 
+
+yum install -y wget && wget -O install.sh http://download.bt.cn/install/install_6.0.sh && bash install.sh
+
+# CentOS 7以下版本
+
+yum install -y wget && wget -O install.sh http://download.bt.cn/install/install.sh && sh install.sh
+
+# Ubuntu
+wget -O install.sh http://download.bt.cn/install/install-ubuntu_6.0.sh && sudo bash install.sh
